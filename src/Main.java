681eb7c2badbf028cc17e8aeea13964fce6493f7
GigiFanariu <139012939+GigiFanariu@users.noreply.github.com>
2023-07-29 12:36:17 +0200

    completato

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("input a number betwen 1 and 10:");
+            int number = scanner.nextInt();
+            switch (number) {
+                case 1 -> System.out.println("one");
+                case 2 -> System.out.println("two");
+                case 3 -> System.out.println("three");
+                case 4 -> System.out.println("four");
+                case 5 -> System.out.println("five");
+                case 6 -> System.out.println("six");
+                case 7 -> System.out.println("seven");
+                case 8 -> System.out.println("eight");
+                case 9 -> System.out.println("nine");
+                case 10 -> System.out.println("ten");
+                default -> {
+                    System.out.println("Cannot give you the name");
+                    return;
+                }
+
+            }
+        }
+        }
+
+}
